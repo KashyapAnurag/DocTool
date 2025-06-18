2e012d7c06fc2034d946662ecf35d8096b578d9c
Anurag  Kashyap <91251342+KashyapAnurag@users.noreply.github.com>
2025-06-18 17:13:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# PDF Manhole Extractor
+
+A Python utility to extract structured data from PDF records and output them into organized Excel spreadsheets.
+
+## Features
+
+- Extracts raw text from PDF pages.
+- Parses incoming and outgoing pipe data.
+- Extracts detailed location and general record information.
+- Outputs data into multi-sheet Excel files per PDF.
+
+## Installation
+
+You can install this package directly from GitHub using `pip`:
+
+```bash
+pip install git+[https://github.com/your_github_username/your_repo_name.git#egg=pdf_manhole_extractor](https://github.com/your_github_username/your_repo_name.git#egg=pdf_manhole_extractor)
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pdf_manhole_extractor', 
+    version='0.1.0',           
+    packages=find_packages(),   
+    
+    # Dependencies of required package to run
+    install_requires=[
+        'pypdf',    # For PDF reading and processing
+        'pandas',   # For DataFrame manipulation
+        'xlsxwriter', # For writing Excel files
+        
+    ],
+    
+    # Metadata 
+    author='Anurag Kashyap',
+    author_email='anuragkr.kashyap@gmail.com',
+    description='A utility to extract structured data from PDF records into Excel.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/KashyapAnurag/DocTool'
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: pdf Processing',
+        'Topic :: Utilities',
+    ],
+    python_requires='>=3.7', 
+)
